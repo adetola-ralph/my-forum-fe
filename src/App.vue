91aef1c5dd302253fd54dbc2d62d2b9f5aadb339
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-08-15 14:24:31 +0100

    General cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
-    <button class="btn btn-default">Submit</button>
   </div>
 </template>
 
@@ -17,8 +15,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
