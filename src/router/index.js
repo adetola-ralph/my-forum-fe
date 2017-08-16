746d1c1f321a28b872f7044e9bef6eb87523a831
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-08-16 13:29:48 +0100

    Add basic structuring for home component

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+// import Hello from '@/components/Hello';
 import Authentication from '@/components/authentication/authentication';
+import Home from '@/components/home/home';
 
 Vue.use(Router);
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: Home,
     },
     {
       path: '/auth',
