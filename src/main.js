c949da0d6501f8b34c3c2a334f5caef706c78dc0
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-10-09 07:29:08 +0100

    Add vee-validate for validation

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue';
 import 'bootstrap-sass';
 import VueCookies from 'vue-cookies';
+import VueValidate from 'vee-validate';
 import App from './App';
 import router from './router';
 import './styles/base.scss';
 
 Vue.config.productionTip = false;
 Vue.use(VueCookies);
+Vue.use(VueValidate);
 
 /* eslint-disable no-new */
 new Vue({
