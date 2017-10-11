705c8f913482a514d902d816d8c6d5635ff54d4f
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-10-11 11:28:07 +0100

    Stub network calls

--- test/unit/specs/authentication.spec.js
+++ test/unit/specs/authentication.spec.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import VueValidate from 'vee-validate';
 import { mount } from 'vue-test-utils';
 import Authentication from '@/components/authentication/authentication';
+import Auth from '@/utilities/auth';
 
-// eslint-disable-next-line
 let wrapper;
 let data;
+let sandbox;
 
 describe('Authentication.vue', () => {
   before(() => {
@@ -87,5 +88,61 @@ describe('Authentication.vue', () => {
       expect(errorPanels.at(0).text().trim()).to.equal('The email field must be a valid email.');
       done();
     }).catch(err => done(err));
+  });
+
+  describe('stubs test', () => {
+    let loginStub;
+    let validateAllStub;
+    before(() => {
+      sandbox = sinon.sandbox.create();
+      loginStub = sandbox.stub(Auth, 'login');
+      loginStub.withArgs({
+        email: 'good@email.ng',
+        password: 'password',
+      }).resolves({
+        token: 'xxxx',
+      });
+      loginStub.withArgs({
+        email: 'bad@email.ng',
+        password: 'password',
+      }).rejects({
+        response: {
+          data: { message: 'Authentication failed: Invalid user' },
+        },
+      });
+      validateAllStub = sandbox.stub(wrapper.vm.$validator, 'validateAll');
+      validateAllStub.resolves(() => {
+        Auth.login();
+      });
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('validator to be called', (done) => {
+      const signinButton = wrapper.find('form.sign-in button');
+      signinButton.trigger('click');
+      Vue.nextTick().then(() => {
+        // eslint-disable-next-line
+        expect(wrapper.vm.$validator.validateAll).to.be.called;
+        done();
+      }).catch(err => done(err));
+    });
+
+    it('should call login method of Auth class for login', (done) => {
+      const signinButton = wrapper.find('form.sign-in button');
+      data.signInObject = {
+        email: 'good@email.ng',
+        password: 'password',
+      };
+      signinButton.trigger('click');
+      Vue.nextTick().then(() => {
+        // eslint-disable-next-line
+        expect(Auth.login).to.be.called;
+        expect(Auth.login).to.be.calledWith(data.signInObject);
+        done();
+      }).catch(err => done(err));
+    });
   });
 });
