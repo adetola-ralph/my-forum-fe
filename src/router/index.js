88221b405a629a0418650037dbffd7a093ce8ac7
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-26 15:19:52 +0100

    Route for topic list component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 import Authentication from '@/components/authentication/authentication';
 import Home from '@/components/home/home';
+import TopicList from '@/components/topic-list/topic-list';
 
 Vue.use(Router);
 
@@ -12,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+      children: [
+        {
+          path: '',
+          component: TopicList,
+        },
+      ],
     },
     {
       path: '/auth',
