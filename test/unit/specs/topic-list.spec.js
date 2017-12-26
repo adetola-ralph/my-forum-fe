86162d66be41ed72493679d60617e77b35c103c2
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-26 20:43:36 +0100

    More tests for topic list

--- test/unit/specs/topic-list.spec.js
+++ test/unit/specs/topic-list.spec.js
@@ -1,19 +1,38 @@
 import { mount, createLocalVue, shallow } from 'vue-test-utils';
 import VueRouter from 'vue-router';
+import moment from 'moment';
 import TopicList from '@/components/topic-list/topic-list';
-// import Topic from '@/utilities/topics';
+import Topic from '@/utilities/topics';
 
 let wrapper;
 let vm;
 let sandbox;
-let getTopicsSpy;
+// let getTopicsStub;
+let getAllTopicStub;
 const Vue = createLocalVue();
 const router = new VueRouter();
+const data = {
+  data: {
+    data: [
+      {
+        id: 1,
+        topicName: 'topicName',
+        userId: 1,
+        createdAt: new Date(2017, 11, 30),
+      },
+      {
+        id: 2,
+        topicName: 'topicName2',
+        userId: 1,
+        createdAt: new Date(2017, 12, 1),
+      },
+    ],
+  },
+};
 
 describe('TopicList.vue', () => {
   before(() => {
     sandbox = sinon.sandbox.create();
-    getTopicsSpy = sandbox.spy();
   });
 
   before(() => {
@@ -22,6 +41,12 @@ describe('TopicList.vue', () => {
       router,
     });
     vm = wrapper.vm;
+    getAllTopicStub = sandbox.stub(Topic, 'getAll');
+    getAllTopicStub.resolves(data);
+  });
+
+  after(() => {
+    sandbox.restore();
   });
 
   it('contains getTopics method', () => {
@@ -36,5 +61,15 @@ describe('TopicList.vue', () => {
 
   it('contains topics as part of data', () => {
     expect(vm).to.have.property('topics');
+  });
+
+  it('', (done) => {
+    vm.getTopics();
+    vm.$nextTick().then(() => {
+      expect(vm.topics[0].id).to.equal(1);
+      expect(vm.topics[0].createdAt).to.equal(moment(data.data.data[0].createdAt)
+        .format('ll'));
+      done();
+    });
   });
 });
