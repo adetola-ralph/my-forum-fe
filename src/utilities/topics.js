71c6ea36b6e33dbaed51f0de4396e930fe7fc336
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-30 14:46:53 +0100

    Retrieve topics and posts under them

--- src/utilities/topics.js
+++ src/utilities/topics.js
@@ -4,4 +4,11 @@ export default class Topics {
   static getAll() {
     return request.get('http://localhost:1236/api/v1/topics?include=users');
   }
+
+  static getOne(topicId) {
+    return Promise.all([
+      request.get(`http://localhost:1236/api/v1/topics/${topicId}?include=users`),
+      request.get(`http://localhost:1236/api/v1/topics/${topicId}/posts?include=users`),
+    ]);
+  }
 }
