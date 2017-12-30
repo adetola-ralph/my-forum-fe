4cf871cdbae01e4868aaaa91ceb4153fa68f0628
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-30 11:26:18 +0100

    Style improvements to topic list

--- src/components/topic-list/topic-list.vue
+++ src/components/topic-list/topic-list.vue
@@ -3,18 +3,26 @@
     <div class="page-header">
       <h1>Topics</h1>
     </div>
-    <div class="topic-list" v-for="topic in topics" :key="topic.id">
-      <p class="topic-list-heading">{{topic.topicName}}</p>
-      <span class="tags-list">
-        <ul class="tags">
-          <li>Tag 1</li>
-          <li>Tag 2</li>
-          <li>Tag 3</li>
-        </ul>
-      </span>
-      <span class="topic-info">
-        by {{topic.author}} at {{topic.createdAt}}
-      </span>
+    <div>
+      <div class="topic-list" v-for="topic in topics" :key="topic.id">
+        <p class="topic-list-heading">{{topic.topicName}}</p>
+        <div class="topic-meta">
+          <span class="topic-info">
+            <a class="author">
+              <img :src="getUserAvatar(topic)" alt="User image" class="author-image"/>
+              {{topic.author}}
+            </a> 
+            at {{topic.createdAt}}
+          </span>
+          <span class="tags-list">
+            <ul class="tags">
+              <li>Tag 1</li>
+              <li>Tag 2</li>
+              <li>Tag 3</li>
+            </ul>
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +50,10 @@ export default {
         });
       });
     },
+    getUserAvatar(topic) {
+      // eslint-disable-next-line
+      return topic.User.profilePicture || require('./../../assets/icons/user-solid-circle.svg');
+    },
   },
   mounted() {
     this.getTopics();
@@ -52,31 +64,56 @@ export default {
 <style lang="scss">
   .topic-list {
     color: #797979;
-    padding: 20px 30px 10px;
-    border-radius: 4px;
+    padding: 20px 20px 10px;
+    // border-radius: 4px;
+    border: #f1f3f5 1px solid;
     position: relative;
-    margin-bottom: 20px;
-    box-shadow: 0 0 8px 0 rgba(0,0,0,.06), 0 1px 0 0 rgba(0,0,0,.02);
+    // box-shadow: 0 0 8px 0 rgba(0,0,0,.06), 0 1px 0 0 rgba(0,0,0,.02);
     background-color: #fff;
+    height: 100px;
+    border-bottom: 0;
+
+    &:first-child {
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+    }
+
+    &:last-child {
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
+      border-bottom: #f1f3f5 1px solid;
+    }
 
     .topic-list-heading {
-      font-size: 20px;
-      font-weight: 800;
-      margin-bottom: 10px;
+      font-size: 19px;
+      font-weight: bold;
+      margin-bottom: 18px;
       box-sizing: border-box;
+      color: #1c2024;
+    }
+
+    .author {
+      color: darken(#85FFBD, 40%);
+
+      &-image {
+        max-width: 17px;
+        border-radius: 50%;
+        vertical-align: sub;
+      }
     }
 
     .tags {
       list-style: none;
       display: inline-block;
-      padding-left: 0;
+      padding-left: 10px;
 
       li {
         display: inline-block;
         text-transform: capitalize;
-        padding: 3px 5px;
+        font-size: 10px;
+        padding: 2px 3px;
         margin-right: 5px;
-        background-color: lighten(#85FFBD, 15%);
+        background-color: lighten(#85FFBD, 20%);
         color: darken(#85FFBD, 30%);
         cursor: pointer;
         transition: background-color 0.5s;
@@ -89,7 +126,10 @@ export default {
     }
 
     .topic-info {
-      float: right;
+      color: #636f7b;
+      font-weight: 400;
+      font-size: 12px;
+
     }
   }
 </style>
