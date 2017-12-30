660239bb72649d94d795d388eca5cad82fa4293b
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-30 14:47:47 +0100

    Add topic detail component

---
+++ src/components/topic-details/topic-details.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="page-header">
+      <h1>{{topic.topicName}}</h1>
+    </div>
+    <div>
+      <div class="lists" v-for="post in posts" :key="post.id">
+        <p class="lists-heading">{{post.content}}</p>
+        <div class="meta">
+          <span class="info">
+            <a class="author">
+              <img :src="getUserAvatar(post)" alt="User image" class="author-image"/>
+              {{post.author}}
+            </a> 
+            at {{post.createdAt}}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import Topics from './../../utilities/topics';
+
+export default {
+  name: 'TopicDetails',
+  props: ['topicId'],
+  data() {
+    return {
+      topic: {},
+      posts: [],
+    };
+  },
+  methods: {
+    getTopicAndPosts() {
+      Topics.getOne(this.topicId).then(([rTopic, rPosts]) => {
+        const topic = rTopic.data.data;
+        const posts = rPosts.data.data;
+
+        this.posts = posts.map((post) => {
+          const modifiedPost = Object.assign({}, post);
+          modifiedPost.createdAt = moment(post.createdAt).format('ll');
+          modifiedPost.author = post.User.name;
+          return modifiedPost;
+        });
+
+        this.topic = Object.assign({
+          createdAt: moment(topic.createdAt).format('ll'),
+          author: topic.User.name,
+        }, topic);
+      });
+    },
+    getUserAvatar(post) {
+      // eslint-disable-next-line
+      return post.User.profilePicture || require('./../../assets/icons/user-solid-circle.svg');
+    },
+  },
+  mounted() {
+    this.getTopicAndPosts();
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
+
