606357f2d841f6dcb7d30603fbd6af6e00fa8f52
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-01 11:25:05 +0100

    Add tests for getOne method in topic utility

--- test/unit/specs/topics.spec.js
+++ test/unit/specs/topics.spec.js
@@ -5,7 +5,7 @@ let sandBox;
 let axiosstub;
 const link = 'http://localhost:1236/api/v1';
 
-const expectedResult = [
+const mockTopics = [
   {
     id: 1,
     topicName: 'topicName',
@@ -20,12 +20,37 @@ const expectedResult = [
   },
 ];
 
+const mockPosts = [
+  {
+    id: 1,
+    content: 'Some content here',
+    userId: 1,
+    createdAt: new Date(),
+    topicId: 2,
+    User: {
+      name: 'Olutola Oreofeoluwapo',
+    },
+  },
+  {
+    id: 2,
+    content: 'Some content here',
+    userId: 1,
+    createdAt: new Date(),
+    topicId: 2,
+    User: {
+      name: 'Olutola Oreofeoluwapo',
+    },
+  },
+];
+
 describe('Topic utility class', () => {
   before(() => {
     sandBox = sinon.sandbox.create();
     axiosstub = sandBox.stub(axios, 'get');
 
-    axiosstub.withArgs(`${link}/topics?include=users`).returns(Promise.resolve(expectedResult));
+    axiosstub.withArgs(`${link}/topics?include=users`).returns(Promise.resolve(mockTopics));
+    axiosstub.withArgs(`${link}/topics/2?include=users`).returns(Promise.resolve(mockTopics[1]));
+    axiosstub.withArgs(`${link}/topics/2/posts?include=users`).returns(Promise.resolve(mockPosts));
   });
 
   after(() => {
@@ -44,7 +69,26 @@ describe('Topic utility class', () => {
 
   it('getAll should return a promise with an array', (done) => {
     Topics.getAll().then((result) => {
-      expect(result).to.equal(expectedResult);
+      expect(result).to.equal(mockTopics);
+      done();
+    });
+  });
+
+  it('should have a getOne funcction', () => {
+    expect(Topics.getOne).to.be.a('function');
+  });
+
+  it('should call axios get', (done) => {
+    Topics.getOne();
+    expect(axios.get).to.have.been.called;
+    done();
+  });
+
+  it('should return a promise with topics and posts', (done) => {
+    Topics.getOne(2).then((res) => {
+      expect(res).to.be.an('array');
+      expect(res[0]).to.equal(mockTopics[1]);
+      expect(res[1]).to.equal(mockPosts);
       done();
     });
   });
